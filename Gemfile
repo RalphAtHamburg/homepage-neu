ed6be6fd402ca6f8d1d1fe0a31da0f38f1dfd2b1
Ralph <Ralph@Ralphs-iMac.fritz.box>
2017-01-30 15:50:26 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+# If you do not have OpenSSL installed, change
+# the following line to use 'http://'
+source 'https://rubygems.org'
+
+
+# Windows does not come with time zone data
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+
+gem 'susy', '>= 2.0.0'
+
+# Middleman Gems
+gem 'middleman', '>= 4.0.0'
+gem 'middleman-livereload'
+gem 'middleman-compass', '>= 4.0.0'
---
+++ config.rb
@@ -0,0 +1,68 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+################################################################################
+# View Helpers
+# Methods defined in the helpers block are available in templates
+################################################################################
+
+helpers do
+  # Calculate the years for a copyright
+  def copyright_years(start_year)
+    end_year = Date.today.year
+    if start_year === end_year
+      start_year
+    else
+      start_year + ' - ' + end_year.to_s
+    end
+  end
+end
+
+################################################################################
+# Configure the directorys
+################################################################################
+
+config[:css_dir] = 'assets/stylesheets'
+config[:js_dir] = 'assets/javascripts'
+config[:images_dir] = 'assets/images'
+config[:fonts_dir] = 'assets/webfonts'
+config[:partials_dir] = 'partials'
+
+configure :development do
+  set :debug_assets, true
+end
+
+configure :build do
+  activate :relative_assets
+end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+end
